974e62bebb7a980ea03954c0c79a5eca256f9a64
HichamHajla <hichem500@hotmail.com>
2022-12-06 09:07:37 +0100

    iteration 5 non terminée

--- src/movies.js
+++ src/movies.js
@@ -1,19 +1,40 @@
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    const newArray = moviesArray.map(element => element.director)
+    return newArray
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    const array2 = moviesArray.filter(element => element.director === "Steven Spielberg" && element.genre.includes("Drama"))
+      return array2.length 
+    }
+    
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    if (!moviesArray.length) {return 0}
+    const noScore = moviesArray.filter(e => e.score)
+    const scoreAvg = noScore.reduce((a ,b) => (a + b.score), 0) / moviesArray.length
+    const roundAverage = scoreAvg.toFixed(2)
+    return Number(roundAverage)
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+    const dramaScore = moviesArray.filter(e => e.genre.includes("Drama"))
+    return scoresAverage(dramaScore)
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    const yearFirst = moviesArray.map(e => e.year)
+    const yearSorted = 
+    
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
